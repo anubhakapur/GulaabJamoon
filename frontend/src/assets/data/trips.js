863f5031c1c96cbed2797d6ccc22eca03f72d803
simran2502 <simssimran2003@gmail.com>
2024-08-25 22:56:34 +0530

    Frontend changes 2

--- frontend/src/assets/data/trips.js
+++ frontend/src/assets/data/trips.js
@@ -35,7 +35,7 @@ const trips = [
     id: 1,
     name: "Beach Paradise",
     description: "Relax on pristine beaches",
-    price: 999,
+    price: 1000,
     image:
       "https://static.vecteezy.com/system/resources/previews/017/521/673/large_2x/amazing-summer-beach-background-sunset-landscape-in-square-format-perfect-coast-view-sunny-relaxation-vibes-chairs-and-umbrella-luxury-summer-beach-idyllic-tropical-island-view-photo.jpg",
   },
